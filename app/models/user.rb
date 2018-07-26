7fe279cea4877bcb96fd354bd707b2a8c72dbb13
Mbah Clinton <clintonmbah44@gmail.com>
2018-07-26 09:25:41 +0000

    added create_jwt to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
+require "jwt"
 class User < ApplicationRecord
     validates_presence_of :first_name
     validates_presence_of :last_name
     validates_presence_of :email
     validates_uniqueness_of :email
     validates_presence_of :password
+
+    def create_jwt
+        payload = {data: {user_id: self.id}}
+        JWT.encode(payload, ENV["jwt_secret"], true, {algorithm : "HS256"})
+    end
 end
