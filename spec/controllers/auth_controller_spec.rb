e48b7cea80a46b416fb4f94ce52ea7d7d169afb1
Mbah Clinton <clintonmbah44@gmail.com>
2018-07-26 10:26:08 +0000

    added auth controller tests

--- spec/controllers/auth_controller_spec.rb
+++ spec/controllers/auth_controller_spec.rb
@@ -1,5 +1,21 @@
-require 'rails_helper'
+require "rails_helper"
+require "json"
 
 RSpec.describe AuthController, type: :controller do
+    describe "POST #signup" do
+        it "will signup a valid user" do
+            user = {
+                first_name: Forgery("name").first_name,
+                last_name: Forgery("name").last_name,
+                email: Forgery("internet").email_address,
+                password: Forgery("basic").password,
+            }
 
+            post :signup, params: user
+
+            expect(response).to have_http_status(201)
+            expect(JSON.parse(response.body)["user"]["first_name"]).to eq(user[:first_name])
+            expect(JSON.parse(response.body)["token"].present?).to eq(true)
+        end
+    end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
     factory :user do
       first_name Forgery("name").first_name
       last_name  Forgery("name").last_name
-      email Forgery("email").address
+      email Forgery("internet").email_address
       password Forgery("basic").password
     end
   end
