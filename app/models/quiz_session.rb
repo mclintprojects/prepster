c34a52b7e59129a970a806c81856c1224dce053b
Mbah Clinton <clintonmbah44@gmail.com>
2018-07-27 18:15:49 +0000

    added quiz_session tests

--- app/models/quiz_session.rb
+++ app/models/quiz_session.rb
@@ -1,2 +1,5 @@
 class QuizSession < ApplicationRecord
+    validates_presence_of :player
+    validates_presence_of :score
+    belongs_to :quiz
 end
--- spec/models/quiz_session_spec.rb
+++ spec/models/quiz_session_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe QuizSession, type: :model do
+    it {should validate_presence_of :player}
+    it {should validate_presence_of :score}
+    it {should belong_to :quiz}
 end
