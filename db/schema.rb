d060e2a0ded65adb19540131384368896acf1703
Mbah Clinton <clintonmbah44@gmail.com>
2018-07-27 17:53:16 +0000

    added answer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_27_174737) do
+ActiveRecord::Schema.define(version: 2018_07_27_175206) do
+
+  create_table "questions", force: :cascade do |t|
+    t.string "text"
+    t.integer "answer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "quiz_sessions", force: :cascade do |t|
     t.string "player"
