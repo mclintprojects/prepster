bf373fdfa59d7b139f4e3404daf6200579a0c3f3
Mbah Clinton <clintonmbah44@gmail.com>
2018-07-27 18:26:28 +0000

    added question tests

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ApplicationRecord
+    validates_presence_of :text
+    validates_presence_of :answer_id
+    has_many :answer
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+    it {should validate_presence_of :text}
+    it {should validate_presence_of :answer_id}
+    it {should have_many :answer}
 end
