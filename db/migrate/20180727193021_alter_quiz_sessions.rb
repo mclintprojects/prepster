1af5cba7c0071e6e54bf0ef59f4752934f9e9d1e
Mbah Clinton <clintonmbah44@gmail.com>
2018-07-28 13:41:26 +0000

    sdsdsd

--- db/migrate/20180727193021_alter_quiz_sessions.rb
+++ db/migrate/20180727193021_alter_quiz_sessions.rb
@@ -1,5 +1,5 @@
 class AlterQuizSessions < ActiveRecord::Migration[5.2]
   def change
-    change_column :quizzes, :sessions, 'integer USING CAST(column_name AS integer)'
+    change_column :quizzes, :sessions, :integer, using: 'sessions::integer'
   end
 end
